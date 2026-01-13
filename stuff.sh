2ca987f1faf475155cd4e2e9f1a8c2041c5a8538
nomad <noahbuergler@proton.me>
2026-01-13 20:39:34 +0100

    stuffstuff

--- stuff.sh
+++ stuff.sh
@@ -1,1 +1,3 @@
 echo -e "\033[0;32;1m\n|\033[0;30;42;1m NEW \033[0;32;1m|\033[0;39m \033[0;39;2m17:34\033[0;39m   \033[0;39;1m+\033[0;39m lol.rs\033[0m"
+echo
+echo -e "\033[0;34m \033[0;30;44;1m DOWNLOADING \033[0;30;44m▌▌▌\033[0;39m  Backend downloads assets\n\n\033[0;34m ▌\033[0;30;44;1m TRANSFERING \033[0;30;44m▌▌\033[0;39m  Transfer assets to client\n\n\033[0;34m ▌▌\033[0;30;44;1m INSTALLING \033[0;30;44m▌\033[0;39m  Client installs assets\n\n\033[0;34m ▌▌▌\033[0;30;44;1m VERIFYING \033[0;39m   Client verifies installation\033[0;30;44;1m\n\n\033[0;34m \033[0;32m▌▌▌▌ COMPLETE\033[0m"
